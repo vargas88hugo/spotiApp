aff7fd208bd9128e77aed323df77833b43e2feda
Hugo Vargas <vargas88hugo@gmail.com>
2019-12-26 15:39:59 -0500

    [Feature] created search component and implemented search engine

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private spotify: SpotifyService) { }
+
+  artists: any[] = [];
 
   ngOnInit() {
+  }
+
+  search(term: string) {
+    console.log(term);
+    this.spotify.getArtist(term)
+                .subscribe((data: any) => {
+                  console.log(data.artists.items);
+                  this.artists = data.artists.items;
+                });
   }
 
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -13,9 +13,18 @@ export class SpotifyService {
   getNewReleases() {
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQBOeU0lefNJHkBiDeTPBXcl0V6nHww9opoPoj_6F1uUebYMthnrifciL5HuKSlX5ezI1Zl0seIarLRKUTk'
+      'Authorization': 'Bearer BQA876WdFAxQexbWl9fivVyaLUQVWAAWELOzpLtfIlXVES-aLRlvwGStmUhdKFEEzcERvwGjAZZz4BBeIS4'
     });
 
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers});
+  }
+
+  getArtist(term: string) {
+    
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQA876WdFAxQexbWl9fivVyaLUQVWAAWELOzpLtfIlXVES-aLRlvwGStmUhdKFEEzcERvwGjAZZz4BBeIS4'
+    });
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ term }&type=artist`, {headers});
   }
 }
